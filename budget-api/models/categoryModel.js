35a9b1a1d9897fe3503830d3c0f3f9f967d2c1b1
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-07 10:31:19 -0300

    category model created

---
+++ budget-api/models/categoryModel.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../database/db");
+const User = require("./userModel.js");
+
+class Category extends Model {}
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "categories" },
+);
+
+Categories.User = Categories.belongsTo(User);
+
+module.exports = Category;
