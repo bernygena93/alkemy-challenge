860f65819fa4b4c9ee3d217640e80b9b406d3a81
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-07 17:05:19 -0300

    react router dom installed & login and registration pages linked

--- budget-app/src/views/layout/Layout.js
+++ budget-app/src/views/layout/Layout.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import styles from "./layout.module.css";
 
 function Layout() {
   return (
     <>
       <nav className={styles.container}>
-        <h2 className={styles.title}>Budget-Managment</h2>
+        <Link to="/" className={styles.link}>
+          <h2 className={styles.title}>Budget-Managment</h2>
+        </Link>
         <div className={styles.containerButton}>
-          <button type="button" className={styles.button}>
-            Ingresar
-          </button>
-          <button type="button" className={styles.button}>
-            Registrarse
-          </button>
+          <Link to="/login" className={styles.link}>
+            <button type="button" className={styles.button}>
+              Ingresar
+            </button>
+          </Link>
+          <Link to="/register" className={styles.link}>
+            <button type="button" className={styles.button}>
+              Registrarse
+            </button>
+          </Link>
         </div>
       </nav>
       <Outlet />
