649ece0923a2647df4f457b2aaa93d925f0d6b88
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-07 10:05:18 -0300

    database configured & test connection success

--- budget-api/server.js
+++ budget-api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const sequelize = require("./database/db");
 
 app.set("port", process.env.PORT || 8080);
 
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), async () => {
   console.log(`app listening on port ${app.get("port")}!`);
+  try {
+    await sequelize.sync({ force: false });
+    console.log("Connection success");
+  } catch (err) {
+    console.log("Connection falied", err);
+  }
 });
