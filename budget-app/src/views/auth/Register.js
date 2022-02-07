0ccce0a618ccda32e009fc35ec79439cd05379ef
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-07 15:27:57 -0300

    forms register & login created

---
+++ budget-app/src/views/auth/Register.js
@@ -0,0 +1,54 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { useForm } from "react-hook-form";
+import styles from "../form.module.css";
+
+function Register() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <label className={styles.label} htmlFor="username">
+          Nombre de usuario
+        </label>
+        <input
+          className={styles.input}
+          id="username"
+          type="text"
+          name="username"
+          {...register("username")}
+        />
+        <label className={styles.label} htmlFor="email">
+          Correo Electronico
+        </label>
+        <input
+          className={styles.input}
+          id="email"
+          type="email"
+          name="email"
+          {...register("email")}
+        />
+        <label className={styles.label} htmlFor="password">
+          Contraseña
+        </label>
+        <input
+          className={styles.input}
+          id="paswword"
+          type="password"
+          name="password"
+          {...register("password")}
+        />
+        <button className={styles.button} type="submit">
+          Registrarse
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
