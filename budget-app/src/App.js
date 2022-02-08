d42c23bb745a828fa56afeb289c8e8c656b5e49a
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-08 12:36:38 -0300

    authentication added to routes

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./views/auth/Login";
 import Register from "./views/auth/Register";
 import Layout from "./views/layout/Layout";
 import Operation from "./views/operation/Operation";
 import Dashboard from "./views/dashboard/Dashboard";
+import BudgetContext from "./context/BudgetContext";
 
 function App() {
+  const context = useContext(BudgetContext);
+  const { islogged } = context;
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/operation/:id" element={<Operation />} />
+          <Route
+            index
+            element={islogged ? <Dashboard /> : <Navigate to="login" />}
+          />
+          <Route
+            path="login"
+            element={!islogged ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="register"
+            element={!islogged ? <Register /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/operation/:id"
+            element={islogged ? <Operation /> : <Navigate to="login" />}
+          />
+          <Route path="*" element={<Navigate replace to="/" />} />
         </Route>
       </Routes>
     </BrowserRouter>
