7dcacb2e388cdb0dd67754a663f7ab69230b11b9
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-07 21:58:06 -0300

    config cross-origin

--- budget-api/server.js
+++ budget-api/server.js
@@ -8,6 +8,12 @@ app.set("port", process.env.PORT || 8080);
 app.set("secretKey", process.env.SECRET_KEY);
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-access-token");
+  next();
+});
 
 app.listen(app.get("port"), async () => {
   try {
