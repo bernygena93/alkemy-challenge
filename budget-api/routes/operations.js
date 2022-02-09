6e444b34ed0b76e27f91643f0fe915e7afdbcad7
bernygena93 <bernygena93@users.noreply.github.com>
2022-02-09 12:49:26 -0300

    routes authenticated with jwt

--- budget-api/routes/operations.js
+++ budget-api/routes/operations.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const req = require("express/lib/request");
 const routes = express.Router();
 const operationController = require("../controllers/operationController");
 
 routes.get("/user/:id", operationController.getAll);
 routes.get("/:id", operationController.getById);
-routes.post("/", operationController.save);
+routes.post(
+  "/",
+  (req, res, next) => {
+    req.app.validateUser(req, res, next);
+  },
+  operationController.save,
+);
 routes.put("/", operationController.update);
 routes.delete("/:id", operationController.delete);
 
--- budget-api/server.js
+++ budget-api/server.js
@@ -27,3 +27,20 @@ const operationsRoutes = require("./routes/operations");
 app.use("/auth", usersRoutes);
 app.use("/categories", categoriesRoutes);
 app.use("/operations", operationsRoutes);
+
+function validateUser(req, res, next) {
+  jwt.verify(
+    req.headers["x-access-token"],
+    req.app.get("secretKey"),
+    function (err, decoded) {
+      if (err) {
+        res.status(401).json({ message: err.message });
+      } else {
+        console.log(decoded);
+        req.body.tokenData = decoded;
+        next();
+      }
+    },
+  );
+}
+app.validateUser = validateUser;
